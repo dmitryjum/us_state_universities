6bc5063af01672299fcb1ae88bc68716b7127f71
Dmitry Jum <xfiler@ymail.com>
2015-11-09 23:21:14 -0500

    changed routes to api contraint and added swagger docs gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
-  namespace :api do
-    namespace :v1 do
-      resources :schools, only: :index do
-        collection do
-          get :top_twenty_keys
+  constraints subdomain: 'api' do
+    namespace :api, path: '/' do
+      namespace :v1 do
+        resources :schools, only: :index do
+          collection do
+            get :top_twenty_keys
+          end
         end
       end
     end
