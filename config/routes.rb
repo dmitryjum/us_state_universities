e4758cb67841fbca1bb2195303528a44a3bbeeca
Dmitry Jum <xfiler@ymail.com>
2015-11-09 23:25:06 -0500

    brough back to working state for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
-    namespace :api, path: '/' do
-      namespace :v1 do
-        resources :schools, only: :index do
-          collection do
-            get :top_twenty_keys
-          end
+
+  namespace :api do
+    namespace :v1 do
+      resources :schools, only: :index do
+        collection do
+          get :top_twenty_keys
         end
       end
     end
