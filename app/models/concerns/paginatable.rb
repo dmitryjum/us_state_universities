8111134e98641c4f5e7b5d223e53f6a7ee5426f6
Dmitry Jum <xfiler@ymail.com>
2019-02-27 23:19:32 -0500

    adds case to where_params_are method in School model

--- app/models/concerns/paginatable.rb
+++ app/models/concerns/paginatable.rb
@@ -2,13 +2,13 @@ module Paginatable
   extend ActiveSupport::Concern
 
   included do
-    scope :paginate, -> (params) do
-    	per_page = params[:per_page]
-    	page = params[:page]
-    	entries_count = School.count
+    scope :paginate, -> (params = {}) do
+      per_page = params[:per_page].nil? ? 10 : params[:per_page]
+      page = params[:page].nil? ? 1 : params[:per_page]
+    	entries_count = self.count
     	pages_per_limit = entries_count / per_page + 1
     	{
-    		records: School.offset((page - 1) * per_page).order('title ASC').limit(per_page),
+    		records: self.offset((page - 1) * per_page).order('title ASC').limit(per_page),
     		entries_count: entries_count,
     		pages_per_limit: pages_per_limit,
     		page: page
--- app/models/school.rb
+++ app/models/school.rb
@@ -13,6 +13,8 @@ class School < ApplicationRecord
       where_title_is params[:title]
     elsif params[:details].present?
       params[:details].is_a?(ActionController::Parameters) ? where_details_are(params[:details].as_json) : where_details_key_is(params[:details])
+    elsif params[:per_page].present? || params[:page].present?
+      paginate(params[:per_page], params[:page])
     else
       all
     end
