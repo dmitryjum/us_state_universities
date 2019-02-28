cc8319ef8791b9c1353984a73e8ae4f9c973f797
Dmitry Jum <xfiler@ymail.com>
2019-02-28 02:58:07 -0500

    adds pagination documentation, user create, login and school update, and readme are still to do

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -6,7 +6,11 @@ class Api::V1::SchoolsController < ApplicationController
   param :title, String, :desc => "Find school by arbitrary title \n {'title' => 'Conn'}, or '/api/v1/schools?title=conn'"
   param :details, ["String", "Hash"], :desc => "Find all schools that contain your specific key in their 'details' attribute \n {'details' => 'website'}, or '/api/v1/schools?details=website'"
   param :details_key, Hash,
-   :desc => "Find all schools by specific key and arbitrary value in its 'details' attribute. \n eg: {'details' => {'location' => 'el paso'}}, or '/api/v1/schools?details%5Blocation%5D=el+paso'"
+    :desc => "Find all schools by specific key and arbitrary value in its 'details' attribute. \n eg: {'details' => {'location' => 'el paso'}}, or '/api/v1/schools?details%5Blocation%5D=el+paso'"
+  param :page, ["String", "Integer"],
+    :desc => "Show specific page. It pairs with 'per_page' param that limits records count per page. If requested by itself, 9 records per page limit will be used. Response will contain 4 keys: 'records', 'entries_count', 'pages_per_limit', 'page' \n {'page' => '4', 'per_page' => '5'}, or '/api/v1/schools?page=4&per_page=5'"
+  param :per_page, ["String", "Integer"],
+    :desc => "Limits count of records requested per specific page. It goes along 'page' param. If requested by itself, 1st page will be returned with requested limit. Response will contain 4 keys: 'records', 'entries_count', 'pages_per_limit', 'page' \n {'page' => '4', 'per_page' => '5'}, or '/api/v1/schools?page=4&per_page=5'"
   example "{\"id\":174,\n \"title\":\"Connecticut State Universities\",\n \"details\":\n  {\"type\":\"Public university system\",\n  \"motto\":\"Qui Transtulit Sustinet\",\n  \"website\":\"http://www.ct.edu\",\n  \"location\":\"Hartford, Connecticut, United States, Coordinates: 41°46′12″N 72°42′03″W﻿ / ﻿41.77007°N 72.70088°W﻿ / 41.77007; -72.70088\",\n  \"students\":\"34,824 (2012)\",\n  \"postgraduates\":\"5,516 (2012)\",\n  \"undergraduates\":\"29,308 (2012)\"},\n \"created_at\":\"2015-10-22T01:15:57.264-04:00\",\n \"updated_at\":\"2015-10-26T00:39:07.133-04:00\"}"
   def index
     @schools = School.where_params_are params
