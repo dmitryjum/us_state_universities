0451d27fa24a30469e5a627b0b3aab1ab09b02bb
Dmitry Jum <xfiler@ymail.com>
2019-03-19 22:50:51 -0400

    paginate returns a hash with records and other pagination props even if page params weren't passed

--- app/models/concerns/paginatable.rb
+++ app/models/concerns/paginatable.rb
@@ -3,20 +3,16 @@ module Paginatable
 
   included do
     scope :paginate, -> (params = {}) do
+      response = {records: self, entries_count: self.count, pages_per_limit: 1, page: 1}
       unless params[:per_page].nil? && params[:page].nil?
         per_page = params[:per_page].nil? ? 9 : params[:per_page].to_i
-        page = params[:page].nil? ? 1 : params[:page].to_i
-      	entries_count = self.count
-      	pages_per_limit = entries_count / per_page + 1
-        records = entries_count <= per_page ? self.all.order('title ASC') :
+        response[:page] = params[:page].nil? ? 1 : params[:page].to_i
+      	resonse[:pages_per_limit] = response[:entries_count] / per_page + 1
+        response[:records] = response[:entries_count] <= per_page ? self.all.order('title ASC') :
                   self.offset((page - 1) * per_page).order('title ASC').limit(per_page)
-      	{
-      		records: records,
-      		entries_count: entries_count,
-      		pages_per_limit: pages_per_limit,
-      		page: page
-      	}
+        response
       end
+      response
     end
   end
 end
