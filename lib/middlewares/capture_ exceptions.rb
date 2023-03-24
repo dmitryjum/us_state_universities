e7d561ca8ba437741bd6fb4c2a797906eb60d468
Dmitry Jum <xfiler@ymail.com>
2023-03-24 01:00:01 -0700

    adds the error reporting middleware. Client work is next

---
+++ lib/middlewares/capture_ exceptions.rb
@@ -0,0 +1,50 @@
+require 'net/http'
+class CaptureExceptions
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    begin
+      @app.call(env)
+    rescue StandardError => e
+      @exception = e
+      capture_exceptions(env)
+      raise e
+    end
+  end
+
+  private
+
+  def capture_exceptions(env)
+    uri = URI('http://localhost:3000/record_exceptions')
+    Net::HTTP.post_from(uri,
+      message: @exception.message,
+      backtrace: @exception.backtrace[0..5].join("\n")
+    )
+  end
+
+  def source_location
+    lines[start_line..end_line].join
+  end
+
+  def source
+    @exception.backtrace.first.split(:)
+  end
+
+  def file_location
+    source[0]
+  end
+
+  def start_line
+    [source[1].to_i - 5, 0].max
+  end
+
+  def end
+    source[1].to_i + 5
+  end
+
+  def lines
+    File.readLines(file_location)
+  end
+end
