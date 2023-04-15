c854c9acba7b9b17b54824819d30574b1b0e99ca
Dmitry Jum <xfiler@ymail.com>
2023-04-14 18:52:34 -0700

    Adds my custom bugtracker gem

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# require './lib/middlewares/main.rb'
+require 'bugtrapper'
 
 module UsStateUniversities
   class Application < Rails::Application
--- lib/middlewares/main.rb
+++
@@ -1,1 +0,0 @@
-require_relative 'capture_exceptions.rb'
