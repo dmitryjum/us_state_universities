3cbff527da04478990cc46fd3154d5fe8656cdf0
Dmitry Jum <xfiler@ymail.com>
2023-05-07 17:32:15 -0700

    updates bugtrapper gem and its setup

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module UsStateUniversities
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.middleware.use BugTrapper::TrapExceptions
+    config.middleware.use BugTrapper::TrapExceptions, app_id: "us_state_universities"
   end
 end
